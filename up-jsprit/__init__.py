1eb5d8a5490d7d7d098a4cbf94a137b00d069cb5
ppablo78 <ppablo78@users.noreply.github.com>
2023-09-26 10:39:01 +0200

    Create __init__.py
    First Commit

---
+++ up-jsprit/__init__.py
@@ -0,0 +1,6 @@
+"""
+up-jsprit: A package for solving vehicle routing problems using JSprit.
+"""
+
+# Import everything from the main module to make it directly accessible.
+from .up-jsprit import *
