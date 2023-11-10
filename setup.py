031538c17705d8a367b549b7017e0e27b7ceab6b
ppablo78 <ppablo78@users.noreply.github.com>
2023-11-10 18:17:23 +0100

    Update setup.py
    Update to Version 1.0.4

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='up-jsprit',
-    version='1.0',
-    packages=find_packages(include=['up_jsprit', 'test', 'jar']),
+    name='up_jsprit',
+    version='1.0.4',
+    packages=find_packages(include=['up_jsprit', 'jar', 'output']),
+    package_data={
+        'up_jsprit': ['jar/*.jar'],
+    },
     install_requires=[
-        'jpype1',
-        'unified_planning',
-        'typing' 
+        'jpype1', 'unified_planning', 'typing', 'requests', 'folium', 'polyline' 
     ],
     author='Paolo Petrinca',
-    author_email='up-jsprit@gmail.com',
+    author_email='paolo.petrinca@gmail.com',
     description='Liabray for Integration of JSprit toolkit with Unified Planning Library',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/ppablo78/up-jsprit',  # if you have a github repo
+    url='https://github.com/ppablo78/up-jsprit',
+        classifiers=[
+        'Programming Language :: Python :: 3',
+        # other classifiers
+    ],
 )
